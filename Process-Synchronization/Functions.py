05dfeabb96cffbec7f6fa82a45c14ce2fe5a4f72
Alireza Saei <saeialireza359@gmail.com>
2023-01-11 23:14:23 +0330

    Functions added

--- Process-Synchronization/Functions.py
+++ Process-Synchronization/Functions.py
@@ -1,11 +1,51 @@
 from time import sleep
 
+
 def add(num, value):
     tmp = 0
     while True:
-        print('add')
+        print('Add')
         num.value += value
         tmp = num.value
         sleep(1)
         if tmp != num.value:
-            print("Process conflict")
+            print('Process Conflict! (ADD)')
+
+
+def sub(num, value):
+    tmp = 0
+    while True:
+        print('Sub')
+        num.value -= value
+        tmp = num.value
+        sleep(1.5)
+        if tmp != num.value:
+            print('Process Conflict! (SUB)')
+
+
+def mul(num, value):
+    tmp = 0
+    while True:
+        print('Mul')
+        num.value *= value
+        tmp = num.value
+        sleep(2)
+        if tmp != num.value:
+            print('Process Conflict! (MUL)')
+
+
+def div(num, value):
+    tmp = 0
+    while True:
+        num.value /= value
+        tmp = num.value
+        print(num.value)
+        sleep(2.5)
+        if tmp != num.value:
+            print('Process Conflict! (DIV)')
+
+
+def show(num):
+    while True:
+        sleep(0.5)
+        print(num.value)
--- Process-Synchronization/main.py
+++ Process-Synchronization/main.py
@@ -1,19 +1,18 @@
-from multiprocessing import Process, Value, Array, Queue, Lock
+from multiprocessing import Process, Value, Array, Queue
 from Functions import *
 from time import sleep
 
 if __name__ == '__main__':
     num = Value('d', 0.0)
     arr = Array('i', range(2))
-    lock = Lock()
 
     q = Queue()
-    p1 = Process(target=add, args=(lock, num, 10))
-    p2 = Process(target=sub, args=(lock, num, 5))
-    p3 = Process(target=mul, args=(lock, num, 2))
-    p4 = Process(target=div, args=(lock, num, 4))
+    p1 = Process(target=add, args=(num, 10))
+    p2 = Process(target=sub, args=(num, 5))
+    p3 = Process(target=mul, args=(num, 2))
+    p4 = Process(target=div, args=(num, 4))
 
-    show = Process(target=show, args=(lock, num,))
+    show = Process(target=show, args=(num,))
     sleep(1)
 
     p1.start()
