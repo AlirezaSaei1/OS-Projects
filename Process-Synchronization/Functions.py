f5d4d28c856d217bdc302e99b9615b6c47eaa802
Alireza Saei <saeialireza359@gmail.com>
2023-01-21 21:48:32 +0330

    Mutex using Semaphores

--- Process-Synchronization/Functions.py
+++ Process-Synchronization/Functions.py
@@ -1,72 +1,57 @@
 from time import sleep
 
 
-def add(lock, num, value):
+def add(sem, num, value):
     tmp = 0
     while True:
-        lock.acquire()
-
-        print(f'Add {num.value} with {value} = {num.value + value}')
-        num.value += value
-        tmp = num.value
-        sleep(1)
-        if tmp != num.value:
-            print('Process Conflict! (ADD)')
-
-        lock.release()
+        with sem:
+            print(f'Add {num.value} with {value} = {num.value + value}')
+            num.value += value
+            tmp = num.value
+            sleep(1)
+            if tmp != num.value:
+                print('Process Conflict! (ADD)')
 
 
-def sub(lock, num, value):
+def sub(sem, num, value):
     tmp = 0
     while True:
-        lock.acquire()
-
-        print(f'Subtract {num.value} by {value} = {num.value - value}')
-        num.value -= value
-        tmp = num.value
-        sleep(1.5)
-        if tmp != num.value:
-            print('Process Conflict! (SUB)')
-
-        lock.release()
+        with sem:
+            print(f'Subtract {num.value} by {value} = {num.value - value}')
+            num.value -= value
+            tmp = num.value
+            sleep(1.5)
+            if tmp != num.value:
+                print('Process Conflict! (SUB)')
 
 
-def mul(lock, num, value):
+def mul(sem, num, value):
     tmp = 0
     while True:
-        lock.acquire()
-
-        print(f'Multiply {num.value} by {value} = {num.value * value}')
-        num.value *= value
-        tmp = num.value
-        sleep(2)
-        if tmp != num.value:
-            print('Process Conflict! (MUL)')
-
-        lock.release()
+        with sem:
+            print(f'Multiply {num.value} by {value} = {num.value * value}')
+            num.value *= value
+            tmp = num.value
+            sleep(2)
+            if tmp != num.value:
+                print('Process Conflict! (MUL)')
 
 
-def div(lock, num, value):
+def div(sem, num, value):
     tmp = 0
     while True:
-        lock.acquire()
-
-        print(f'Divide {num.value} by {value} = {num.value / value}')
-        num.value /= value
-        tmp = num.value
-        print(num.value)
-        sleep(2.5)
-        if tmp != num.value:
-            print('Process Conflict! (DIV)')
-
-        lock.release()
+        with sem:
+            print(f'Divide {num.value} by {value} = {num.value / value}')
+            num.value /= value
+            tmp = num.value
+            print(num.value)
+            sleep(2.5)
+            if tmp != num.value:
+                print('Process Conflict! (DIV)')
 
 
-def show(lock, num):
+def show(sem, num):
     while True:
-        lock.acquire()
-
-        sleep(0.5)
-        print(num.value)
-
-        lock.release()
+        with sem:
+            sleep(0.5)
+            print(num.value)
