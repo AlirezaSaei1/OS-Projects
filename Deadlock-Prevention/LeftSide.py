42047e69d9b40fdbb6d5a276ca5de2757ce909fb
Mohammad <tavakolimohammad969@gmail.com>
2023-01-21 23:59:48 +0330

    DeadLock Prevention Using Semaphore

--- Deadlock-Prevention/LeftSide.py
+++ Deadlock-Prevention/LeftSide.py
@@ -1,11 +1,9 @@
-
-from multiprocessing import Process, Queue, Value,Array
+from multiprocessing import Process, Queue, Value, Array
 from time import sleep
 from Car import Car
 
 
-
-def producer(queue, id,):
+def producer(sem, queue, id, ):
     print('Producer: Running', flush=True)
     while True:
         value = Car(id.value)
@@ -14,13 +12,14 @@ def producer(queue, id,):
         queue.put(value)
 
 
-def consumer(queue, street):
+def consumer(sem, queue, street):
     print('Consumer: Running', flush=True)
     while True:
-        item = queue.get()     
-        street.value = item.id
-        print('car id: ', item.id, 'sleep: ', item.time)
-        temp = street.value
-        sleep(item.time)
-        if temp != street.value:
-            print('Process conflict!')
+        with sem:
+            item = queue.get()
+            street.value = item.id
+            print('car id: ', item.id, 'sleep: ', item.time)
+            temp = street.value
+            sleep(item.time)
+            if temp != street.value:
+                print('Process conflict!')
--- Deadlock-Prevention/main.py
+++ Deadlock-Prevention/main.py
@@ -1,29 +1,24 @@
-from multiprocessing import Process, Queue, Value,Array
-import  RightSide, LeftSide
-
+from multiprocessing import Process, Queue, Value, Semaphore
+import RightSide, LeftSide
 
 
 def main():
+    street = Value('d', 0)
+    id = Value('d', 1)
+    sem = Semaphore(1)
 
-    street = Value('d',0)
-    id = Value('d',1)
-    
     car_list1 = Queue(maxsize=10)
     car_list2 = Queue(maxsize=10)
 
-
-    prod1 = Process(target=RightSide.producer, args=(car_list1, id))
-    cons1 = Process(target=RightSide.consumer, args=(car_list1, street))
+    prod1 = Process(target=RightSide.producer, args=(sem, car_list1, id))
+    cons1 = Process(target=RightSide.consumer, args=(sem, car_list1, street))
     prod1.start()
     cons1.start()
 
-
-    prod2 = Process(target=LeftSide.producer, args=(car_list2, id))
-    cons2 = Process(target=LeftSide.consumer, args=(car_list2, street))
+    prod2 = Process(target=LeftSide.producer, args=(sem, car_list2, id))
+    cons2 = Process(target=LeftSide.consumer, args=(sem, car_list2, street))
     prod2.start()
     cons2.start()
-
-
 
 
 if __name__ == '__main__':
