14a5da7d1869e6a36eadee3437c904d64afb5227
Alireza Saei <saeialireza359@gmail.com>
2023-01-22 00:20:47 +0330

    process class added

---
+++ Memory-Management/Process.py
@@ -0,0 +1,4 @@
+class Process():
+    def __init__(self, name, variables) -> None:
+        self.name = name
+        self.variables = variables
--- Memory-Management/main.py
+++ Memory-Management/main.py
@@ -1,27 +1,27 @@
 from MainMemory import MainMemory
+from Process import Process
 
-proccess_vriables = {}
+processes = []
 variable_found = False
 
 
 def main():
-    print(proccess_vriables)
     Mem = MainMemory()
     while (True):
         instr = input().split()
-        if (instr[0] == 'Request'):
+        if (instr[0] == 'r'):
             var_name = instr[1]
-            for key in proccess_vriables.keys():
-                if var_name in proccess_vriables[key].keys():
+            for process in processes:
+                if var_name in process.variables.keys():
                     variable_found = True
 
                     logical_address = Mem.logical(
-                        proccess_vriables[key], var_name)
+                        process.variables, var_name)
                     print(
                         f'Logical: Page {logical_address[0]} - Offset {logical_address[1]}')
 
                     physical_Address = Mem.physical(
-                        proccess_vriables[key], var_name, key)
+                        process.variables, var_name, process.name)
                     print(
                         f'Physical: Frame {physical_Address[0]} - Offset {physical_Address[1]}')
 
@@ -44,12 +44,12 @@ if __name__ == "__main__":
                 if process_name == None:
                     pass
                 else:
-                    proccess_vriables[process_name] = vrs
+                    processes.append(Process(process_name, vrs))
                     vrs = {}
                 process_name = x[1]
 
             else:
                 vrs[x[1]] = int(x[2])
-        proccess_vriables[process_name] = vrs
+        processes.append(Process(process_name, vrs))
 
     main()
